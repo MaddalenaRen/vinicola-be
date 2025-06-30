ef3b29b1c160247ef0f4b338afadc7c3aef11564
MaddalenaRen <ileniarennella@gmail.com>
2025-06-30 11:25:49 +0200

    Initial commit

---
+++ src/main/java/it/epicode/vinicola_be/VinicolaBeApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.vinicola_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinicolaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinicolaBeApplication.class, args);
+	}
+
+}
